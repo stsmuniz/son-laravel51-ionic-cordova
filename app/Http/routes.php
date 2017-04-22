3ccf303449be7e3b00170b7305bbb378c569076e
Salustiano Muniz <smuniz@inone.com.br>
2017-04-22 19:34:04 -0300

    partial fase4

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -70,12 +70,25 @@ Route::group(['prefix' => 'customer', 'as' => 'customer.', 'middleware' => 'auth
 });
 
 Route::group(['prefix' => 'api', 'middleware' => 'oauth', 'as' => 'api.'], function() {
-    Route::get('pedidos', function() {
-        return [
-            'id' => 1,
-            'client' => 'Salustiano',
-            'total' => 10
-        ];
+
+    Route::group(['prefix' => 'client','middleware' => 'oauth.checkrole:client', 'as' => 'client.'], function() {
+        Route::get('pedidos', function() {
+            return [
+                'id' => 1,
+                'client' => 'Salustiano - Cliente',
+                'total' => 10
+            ];
+        });
+    });
+
+    Route::group(['prefix' => 'deliveryman','middleware' => 'oauth.checkrole:deliveryman', 'as' => 'deliveryman.'], function() {
+        Route::get('pedidos', function() {
+            return [
+                'id' => 1,
+                'client' => 'Salustiano - Entregador',
+                'total' => 10
+            ];
+        });
     });
 });
 
